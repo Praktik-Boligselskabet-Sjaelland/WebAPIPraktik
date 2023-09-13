dcfe9026605221182b0f9844bc71dfc1e4b3f5f0
FrejaFG <ferling.gud@gmail.com>
2023-09-13 11:35:05 +0200

    info side oprettet

--- WebApp/Pages/Info.cshtml.cs
+++ WebApp/Pages/Info.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
-    public class PaavirkningModel : PageModel
+    public class InfoModel : PageModel
     {
         public void OnGet()
         {
