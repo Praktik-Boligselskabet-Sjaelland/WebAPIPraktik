d23802915f7daede0391844ea3b00a0f82911cb0
FrejaFG <ferling.gud@gmail.com>
2023-09-13 14:39:01 +0200

    Models folder + device class + fake repo

---
+++ WebApp/Models/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Effect { get; set; }
+        public int RunTime { get; set; }
+
+    }
+}
---
+++ WebApp/Models/FakeDeviceRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models
+{
+    public class FakeDeviceRepository
+    {
+        private List<Device> _devices;
+
+        public FakeDeviceRepository() 
+        {
+            _devices = new List<Device>();
+            _devices.Add(new Device() { Id=1, Name="Vaskemaskine", Description="Bosh", Effect=23, RunTime=7000});
+            _devices.Add(new Device() { Id =2 , Name = "Elbil", Description = "Tesla", Effect =23 , RunTime = 7000});
+            _devices.Add(new Device() { Id = 3, Name = "GamerPC", Description = "Stationer PC", Effect = 23, RunTime = 7000 });
+        }
+    }
+}
